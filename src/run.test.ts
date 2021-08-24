2948a463e960d1d3d0bf5e8b5adc3d2f727acc90
0x-jerry <x.jerry.wang@gmail.com>
2021-08-24 17:03:47 +0800

    perf(run): add run options

--- src/run.test.ts
+++ src/run.test.ts
@@ -1,13 +1,25 @@
 import { assertEquals, assertThrowsAsync } from 'testing/asserts.ts'
-import { runPiped } from './run.ts'
+import { run } from './run.ts'
 
-Deno.test('run result', async () => {
+const runPiped = (...args: string[]) =>
+  run(
+    {
+      stderr: 'inherit',
+      stdin: 'inherit',
+      stdout: 'piped',
+    },
+    ...args,
+  )
+    .then((program) => program.output())
+    .then((output) => new TextDecoder().decode(output).trim())
+
+Deno.test('runPiped result', async () => {
   const output = await runPiped('echo', 'hello')
 
   assertEquals(output.trim(), 'hello')
 })
 
-Deno.test('run throw error', async () => {
+Deno.test('runPiped throw error', async () => {
   await assertThrowsAsync(async () => {
     await runPiped('xxx', 'hello')
   })
--- src/run.ts
+++ src/run.ts
@@ -8,15 +8,75 @@ function getFormatCmd(cmd: string[]) {
   ].join(' ')
 }
 
-export async function run(...cmd: string[]) {
-  console.log(rgb24(getFormatCmd(cmd), 0x999999))
+type RunOptions = Omit<Deno.RunOptions, 'cmd'> & {
+  /**
+   * @default true
+   */
+  log?: boolean
+  /**
+   * @default true
+   */
+  wait?: boolean
+}
 
-  const program = Deno.run({
-    cmd: cmd,
-    stdout: 'inherit',
+/**
+ * ```ts
+ * run({log:false}, 'echo', 'hello')
+ * ```
+ * @param opt
+ * @param cmd
+ */
+export async function run<T extends RunOptions>(
+  opt: T,
+  ...cmd: string[]
+): Promise<Deno.Process<T & { cmd: string[] }>>
+/**
+ * ```ts
+ * run('echo', 'hello')
+ * ```
+ * @param opt
+ * @param cmd
+ */
+export async function run(
+  ...cmd: string[]
+): Promise<Deno.Process<Deno.RunOptions>>
+/**
+ *
+ * @param opt
+ * @param cmd
+ * @returns
+ */
+export async function run(
+  opt: RunOptions | string,
+  ...cmd: string[]
+): Promise<Deno.Process> {
+  const option: RunOptions = {
     stderr: 'inherit',
     stdin: 'inherit',
+    stdout: 'inherit',
+    log: true,
+    wait: true
+  }
+
+  // Is not option
+  if (typeof opt === 'string') {
+    cmd.unshift(opt)
+  } else {
+    Object.assign(option, opt)
+  }
+
+  if (option.log) {
+    console.log(rgb24(getFormatCmd(cmd), 0x999999))
+  }
+
+  const program = Deno.run({
+    ...option,
+    cmd: cmd,
   })
+
+  if (!option.wait) {
+    return program
+  }
 
   const status = await program.status()
   program.close()
@@ -24,24 +84,28 @@ export async function run(...cmd: string[]) {
   if (!status.success) {
     throw status
   }
+
+  return program
 }
 
 const decoder = new TextDecoder()
 
+/**
+ *
+ * @deprecated Use `run` instead of.
+ * @param cmd
+ * @returns
+ */
 export async function runPiped(...cmd: string[]) {
-  const program = Deno.run({
-    cmd: cmd,
-    stdout: 'piped',
-    stderr: 'inherit',
-    stdin: 'inherit',
-  })
-
-  const status = await program.status()
-  program.close()
-
-  if (!status.success) {
-    throw program
-  }
+  const program = await run(
+    {
+      stdout: 'piped',
+      stderr: 'inherit',
+      stdin: 'inherit',
+      log: false,
+    },
+    ...cmd,
+  )
 
   const output = await program.output()
   return decoder.decode(output).trim()
